af3726f8221c24e99da3607e085461ff65befb3a
Richard Hopton <richard@thehoptons.co.uk>
2012-01-24 22:46:49 +0000

    Feature parity with SqlTests from main Simple.Data repo.

---
+++ Simple.Data.SqlAnywhereTest/SchemaTests/DataTypeTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Simple.Data.SqlAnywhereTest.SchemaTests
+{
+    [TestFixture]
+    public class DataTypeTests
+    {
+        [Test]
+        public void TestDecimalCanBeRetrievedCorrectly()
+        {
+            var db = DatabaseHelper.Open();
+            var value = db.DecimalTest.FindById(1).Value;
+            Assert.AreEqual(typeof(Decimal), value.GetType());
+            Assert.AreEqual(1.234567, value);
+        }
+
+        [Test]
+        public void TestDecimalCanBeInsertedCorrectly()
+        {
+            var db = DatabaseHelper.Open();
+            var decimalTest = new { Value = 12.345678 };
+            var value = db.DecimalTest.Insert(decimalTest).Value;
+            Assert.AreEqual(typeof(Decimal), value.GetType());
+            Assert.AreEqual(decimalTest.Value, value);
+        }
+    }
+}
